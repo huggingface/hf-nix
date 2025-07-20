870b2ae1fdaf745f4945cf11cf4182e48f6b48c5
Daniël de Kok <me@danieldk.eu>
2025-07-20 08:59:52 +0000

    python3Packages.torch_2_7: 2.7.0-rc9 -> 2.7.1

--- pkgs/python-modules/torch_2_7/default.nix
+++ pkgs/python-modules/torch_2_7/default.nix
@@ -285,7 +285,7 @@ in
 buildPythonPackage rec {
   pname = "torch";
   # Don't forget to update torch-bin to the same version.
-  version = "2.7.0";
+  version = "2.7.1";
   pyproject = true;
 
   stdenv = effectiveStdenv;
@@ -303,10 +303,9 @@ buildPythonPackage rec {
     owner = "pytorch";
     repo = "pytorch";
     # Switch back after 2.7 is released.
-    #tag = "v${version}";
-    tag = "v2.7.0-rc9";
+    tag = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-4QMLBqD2/0QzwmHqNAyEam6bDsuI6YT9LNBYM62ne2U=";
+    hash = "sha256-wVzYx8YYoL8rVYb9DwF6ai16UzPvSO4WhNvddh09RXM=";
   };
 
   patches =
