e38e81c95b214d87c6100144bc16b3d9923f8c5c
Daniël de Kok <me@danieldk.eu>
2025-08-27 08:11:10 +0000

    python3Packages.torch_2_6: remove

--- overlay.nix
+++ overlay.nix
@@ -212,8 +212,6 @@ rec {
 
         torch = python-self.torch_2_8;
 
-        torch_2_6 = callPackage ./pkgs/python-modules/torch_2_6 { };
-
         torch_2_7 = callPackage ./pkgs/python-modules/torch_2_7 { };
 
         torch_2_8 = callPackage ./pkgs/python-modules/torch_2_8 { };
