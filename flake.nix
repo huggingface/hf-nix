e9495c5465a693ac76963f75f2f31713d0001efd
Wang, Yi A <yi.a.wang@intel.com>
2025-08-29 08:28:48 +0000

    add xpu triton
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
 
             rocm = {
               python3Packages = with pkgsRocm.python3.pkgs; {
-                inherit torch;
+                inherit torch triton-rocm;
               };
             };
           };
